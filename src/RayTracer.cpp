5e5901ad8e830d27d531fb4614935d1191699cd7
SamPal <trashsn0779@gmail.com>
2024-02-20 20:28:10 -0500

    Full Parsing functional

--- src/RayTracer.cpp
+++ src/RayTracer.cpp
@@ -1,10 +1,47 @@
 #include "RayTracer.h"
 #include <iostream>
 #include <fstream>
+/////////////////////////////////////////// LIGHTING CLASS METHODS //////////////////////////////////////////
+Lighting::Lighting(const nlohmann::json& j) {
+    // Mendatory member
+    type = j.at("type").get<std::string>();
+    
+    id = Eigen::Vector3f(j.at("id")[0], j.at("id")[1], j.at("id")[2]);
+    is = Eigen::Vector3f(j.at("is")[0], j.at("is")[1], j.at("is")[2]);
+    
+
+    // Optional member
+    if (j.find("usecenter") != j.end()) {
+        usecenter = j.at("usecenter");
+    }
+    if (j.find("transform") != j.end()) {
+        transform << j.at("transform")[0],j.at("transform")[1],j.at("transform")[2],j.at("transform")[3],
+        j.at("transform")[4],j.at("transform")[5],j.at("transform")[6],
+        j.at("transform")[7],j.at("transform")[8],j.at("transform")[9],j.at("transform")[10],
+        j.at("transform")[11],j.at("transform")[12],j.at("transform")[13],
+        j.at("transform")[14],j.at("transform")[15] ;
+    }
+    if (j.find("n") != j.end()) {
+        n = j.at("n");
+    }
+}
+
+Point::Point(const nlohmann::json& j) : Lighting(j) {
+    centre = Eigen::Vector3f(j.at("centre")[0], j.at("centre")[1], j.at("centre")[2]);
+}
+
+Area::Area(const nlohmann::json& j) : Lighting(j) {
+    p1 = Eigen::Vector3f(j.at("p1")[0], j.at("p1")[1], j.at("p1")[2]);
+    p2 = Eigen::Vector3f(j.at("p2")[0], j.at("p2")[1], j.at("p2")[2]);
+    p3 = Eigen::Vector3f(j.at("p3")[0], j.at("p3")[1], j.at("p3")[2]);
+    p4 = Eigen::Vector3f(j.at("p4")[0], j.at("p4")[1], j.at("p4")[2]);
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////////// OBJECT CLASS METHODS ///////////////////////////////////////////
  
 Object::Object(const nlohmann::json& j) {
+    // Mendatory member
     type = j.at("type").get<std::string>();
     ka = j.at("ka").get<float>();
     kd = j.at("kd").get<float>();
@@ -13,6 +50,19 @@ Object::Object(const nlohmann::json& j) {
     ac = Eigen::Vector3f(j.at("ac")[0], j.at("ac")[1], j.at("ac")[2]);
     dc = Eigen::Vector3f(j.at("dc")[0], j.at("dc")[1], j.at("dc")[2]);
     sc = Eigen::Vector3f(j.at("sc")[0], j.at("sc")[1], j.at("sc")[2]);
+
+    // Optional member
+    if (j.find("comment") != j.end()) {
+        comment = j.at("comment").get<std::string>();
+    }
+    if (j.find("transform") != j.end()) {
+        transform << j.at("transform")[0],j.at("transform")[1],j.at("transform")[2],j.at("transform")[3],
+        j.at("transform")[4],j.at("transform")[5],j.at("transform")[6],
+        j.at("transform")[7],j.at("transform")[8],j.at("transform")[9],j.at("transform")[10],
+        j.at("transform")[11],j.at("transform")[12],j.at("transform")[13],
+        j.at("transform")[14],j.at("transform")[15] ;
+    }
+    
 }
 
 Sphere::Sphere(const nlohmann::json& j) : Object(j) {
@@ -25,6 +75,36 @@ Rectangle::Rectangle(const nlohmann::json& j) : Object(j) {
     p2 = Eigen::Vector3f(j.at("p2")[0], j.at("p2")[1], j.at("p2")[2]);
     p3 = Eigen::Vector3f(j.at("p3")[0], j.at("p3")[1], j.at("p3")[2]);
     p4 = Eigen::Vector3f(j.at("p4")[0], j.at("p4")[1], j.at("p4")[2]);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////// OUTPUT CLASS METHODS ///////////////////////////////////////////
+ 
+Output::Output(const nlohmann::json& j) {
+    // Mendatory member
+    filename = j.at("filename").get<std::string>();
+    size = Eigen::Vector2i(j.at("size")[0], j.at("size")[1]);
+    centre = Eigen::Vector3f(j.at("centre")[0], j.at("centre")[1], j.at("centre")[2]);
+    fov = j.at("fov").get<float>();
+    up = Eigen::Vector3f(j.at("up")[0], j.at("up")[1], j.at("up")[2]);
+    lookat = Eigen::Vector3f(j.at("lookat")[0], j.at("lookat")[1], j.at("lookat")[2]);
+    ai = Eigen::Vector3f(j.at("ai")[0], j.at("ai")[1], j.at("ai")[2]);
+    bkc = Eigen::Vector3f(j.at("bkc")[0], j.at("bkc")[1], j.at("bkc")[2]);
+
+    // Optional member
+    if (j.find("raysperpixel") != j.end()) {
+        raysperpixel = j.at("raysperpixel").get<std::vector<unsigned int>>();
+    }
+    if (j.find("antialiasing") != j.end()) {
+        antialiasing = j.at("antialiasing");
+    }
+    if (j.find("twosiderender") != j.end()) {
+        twosiderender = j.at("twosiderender");
+    }
+    if (j.find("globalillum") != j.end()) {
+        globalillum = j.at("globalillum");
+    }
+    
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -50,33 +130,98 @@ void RayTracer::SavePPM(std::string filename) {
     std::ofstream file(filename);
 }
 
-void RayTracer::Geometry() {
+std::vector<std::unique_ptr<Object>> RayTracer::GeometryParser() {
+    std::vector<std::unique_ptr<Object>> Output;
     for (auto& objectJson : raw_geometry[0]) {
-        
         if (objectJson["type"] == "rectangle") {
-            Rectangle rectangleObject(objectJson);
-            std::cout << "Rectangle Created !" << std::endl;
+            Output.push_back(std::make_unique<Rectangle>(objectJson));
         } else if (objectJson["type"] == "sphere") {
-            Sphere sphereObject(objectJson);
-            std::cout << "Sphere Created !" << std::endl;
+            Output.push_back(std::make_unique<Sphere>(objectJson));
         }
+    }
+    return Output;
+}
+
+
+void RayTracer::GeometryHandler() {
+    for (const auto& objPtr : GeometryParser()) {
+        const Object& obj = *objPtr;
+        std::cout << "Type: " << obj.type << std::endl;
+        if (!obj.comment.empty()) {
+            std::cout << "Comment: " << obj.comment << std::endl;
+        }
+
+        if (obj.type == "rectangle") {
+            const Rectangle& rect = dynamic_cast<const Rectangle&>(obj);
+            // Print details specific to Rectangle, e.g., p1, p2, p3, p4
+        } else if (obj.type == "sphere") {
+            const Sphere& sphere = dynamic_cast<const Sphere&>(obj);
+            // Print details specific to Sphere, e.g., centre, radius
+        }
+        std::cout << "-------------------------" << std::endl;
     }
 }
 
-void RayTracer::Output() {
-    for (auto& output : raw_output[0]) {
-        SavePPM(output["filename"]);
-        std::cout << output["filename"] << " created in build directory." << std::endl;
+std::vector<std::unique_ptr<Lighting>> RayTracer::LightParser() {
+    std::vector<std::unique_ptr<Lighting>> Output;
+    for (auto& objectJson : raw_light[0]) {
+        if (objectJson["type"] == "point") {
+            Output.push_back(std::make_unique<Point>(objectJson));
+        } else if (objectJson["type"] == "area") {
+            Output.push_back(std::make_unique<Area>(objectJson));
+        }
+    }
+    return Output;
+}
+
+
+void RayTracer::LightHandler() {
+    for (const auto& objPtr : LightParser()) {
+        const Lighting& lght = *objPtr;
+        std::cout << "Type: " << lght.type << std::endl;
+
+        if (lght.type == "area") {
+            const Area& area = dynamic_cast<const Area&>(lght);
+            std::cout << "Area : ";
+            std::cout << "p1: " << area.p1 << ", ";
+            std::cout << "p2: " << area.p2 << ", ";
+            std::cout << "p3: " << area.p3 << ", ";
+            std::cout << "p4: " << area.p4 << std::endl;
+        } else if (lght.type == "point") {
+            const Point& point = dynamic_cast<const Point&>(lght);
+            std::cout << "Centre : " << point.centre << std::endl;
+        }
+        std::cout << "-------------------------" << std::endl;
+    }
+}
+
+std::vector<std::unique_ptr<Output>> RayTracer::OutputParser() {
+    std::vector<std::unique_ptr<Output>> result;
+    for (auto& objectJson : raw_output[0]) {
+        result.push_back(std::make_unique<Output>(objectJson));
+    }
+    return result;
+}
+
+void RayTracer::OutputHandler() {
+
+    for (const auto& objPtr : OutputParser()) {
+        const Output& output = *objPtr;
+
+        SavePPM(output.filename);
+        std::cout << output.filename << " created in build directory." << std::endl;
+        std::cout << "-------------------------" << std::endl;
     }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////// RUN METHOD (CALLED BY MAIN.CPP)     /////////////////////////////////
 void RayTracer::run() {
-    std::cout << "-----------------ALPHA 1 - RAYTRACER-------------------------"<< std::endl;
+    std::cout << "-----------------ALPHA 2 - RAYTRACER-------------------------"<< std::endl;
     ProcessJson(data);
-    Geometry();
-    Output();
+    GeometryHandler();
+    LightHandler();
+    OutputHandler();
     
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- src/RayTracer.h
+++ src/RayTracer.h
@@ -5,31 +5,61 @@
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <vector>
+#include <memory>
+
+/////////////////////////////////////////// LIGHTING CLASS METHODS //////////////////////////////////////////
+class Lighting {
+    public:
+    // Constructor
+    explicit Lighting(const nlohmann::json& j);
+
+    // Destructor
+    virtual ~Lighting() = default;
+
+    // Mendatory Members
+    std::string type;
+    Eigen::Vector3f id, is; //Diffuse and specular light intensity
+
+    // Optional Members
+    Eigen::Matrix4f transform; //4x4 affine transformation matrix in row-major representation
+    bool usecenter;
+    unsigned int n;
 
 
-class RayTracer {
-public:
-
-    RayTracer(nlohmann::json& j);
-    void ProcessJson(nlohmann::json& j);
-    void SavePPM(std::string filename);
-    void Output();
-    void Geometry();
-    void run();
-
-private:
-    nlohmann::json data;
-    std::vector<nlohmann::json> raw_geometry;
-    std::vector<nlohmann::json> raw_light;
-    std::vector<nlohmann::json> raw_output;
 };
 
+class Point : public Lighting {
+    public :
+    Eigen::Vector3f centre;
+
+    // Explicit constructor for Point
+    explicit Point(const nlohmann::json& j);
+    
+};
+
+class Area : public Lighting {
+    public :
+    Eigen::Vector3f p1, p2, p3, p4; // Corner points
+    
+    // Explicit constructor for Area
+    explicit Area(const nlohmann::json& j);
+
+};
+
+
+/////////////////////////////////////////// Object CLASS METHODS //////////////////////////////////////////
 class Object {
 public:
+
+    // Mendatory Members
     std::string type;
     float ka, kd, ks; // Ambient, diffuse, and specular reflection coefficients
     float pc; // Phong coefficient
     Eigen::Vector3f ac, dc, sc; // Ambient, diffuse, and specular colors
+
+    // Optional Members
+    std::string comment;
+    Eigen::Matrix4f transform; //4x4 affine transformation matrix in row-major representation
 
     // Default constructor
     Object() = default;
@@ -48,6 +78,7 @@ public:
     Eigen::Vector3f centre;
     // Explicit constructor for Sphere
     explicit Sphere(const nlohmann::json& j);
+
 };
 
 class Rectangle : public Object {
@@ -56,6 +87,63 @@ public:
     
     // Explicit constructor for Rectangle
     explicit Rectangle(const nlohmann::json& j);
+
+};
+/////////////////////////////////////////// Output CLASS METHODS //////////////////////////////////////////
+
+class Output {
+public: 
+
+    // Mendatory members
+    std::string filename;
+    Eigen::Vector2i size;
+    Eigen::Vector3f centre;
+    float fov;
+    Eigen::Vector3f up;
+    Eigen::Vector3f lookat, ai, bkc;
+
+    // Optional members
+    std::vector<unsigned int> raysperpixel;
+    bool antialiasing, twosiderender, globalillum;
+    
+    // Constructor
+    explicit Output(const nlohmann::json& j);
+
+    // Destructor
+    virtual ~Output() = default;
+
+};
+
+/////////////////////////////////////////// RayTracer CLASS METHODS //////////////////////////////////////////
+
+
+class RayTracer {
+public:
+
+    RayTracer(nlohmann::json& j);
+   
+    void SavePPM(std::string filename);
+    
+    
+    void run();
+
+    // Parser Functions (Returns vector of said Class)
+    void ProcessJson(nlohmann::json& j);
+    std::vector<std::unique_ptr<Object>> GeometryParser(); 
+    std::vector<std::unique_ptr<Lighting>> LightParser();
+    std::vector<std::unique_ptr<Output>> OutputParser();
+
+
+    // Handler Functions
+    void GeometryHandler();
+    void LightHandler();
+    void OutputHandler();
+
+private:
+    nlohmann::json data;
+    std::vector<nlohmann::json> raw_geometry;
+    std::vector<nlohmann::json> raw_light;
+    std::vector<nlohmann::json> raw_output;
 };
 
 
